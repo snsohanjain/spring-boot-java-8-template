750e17f8707195af55ca35a3bc7a24f8c20f4528
Sohan Jain S N <snsohanjain@gmail.com>
2023-03-17 20:59:04 +0530

    first commit

---
+++ src/test/java/com/letpay/easypay/EasypayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letpay.easypay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasypayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
